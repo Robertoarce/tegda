5fa3407aa818b6fe1dc99cfd16595fdba9233f49
Roberto Arce <roberto_arce_@hotmail.com>
2023-05-20 14:57:53 +0200

    comment out

--- graphs/outliers/detection.py
+++ graphs/outliers/detection.py
@@ -1,26 +1,26 @@
 
-def create_
+# def create_
 
-# Create a list of consecutive integers
-integers = range(len(prices))
+# # Create a list of consecutive integers
+# integers = range(len(prices))
 
-plt.figure(figsize=(16, 8))
+# plt.figure(figsize=(16, 8))
 
-# Plot a scatterplot
-plt.scatter(y=prices, x=integers, c='red', alpha=0.5)
-plt.show()
+# # Plot a scatterplot
+# plt.scatter(y=prices, x=integers, c='red', alpha=0.5)
+# plt.show()
 
------
-# Import the zscores function
-from scipy.stats import zscore
+# -----
+# # Import the zscores function
+# from scipy.stats import zscore
 
-# Find the zscores of prices
-scores = zscore(prices)
+# # Find the zscores of prices
+# scores = zscore(prices)
 
-# Check if the absolute values of scores are over 3
-is_over_3 = abs(scores)>3
+# # Check if the absolute values of scores are over 3
+# is_over_3 = abs(scores)>3
 
-# Use the mask to subset prices
-outliers = prices[is_over_3]
+# # Use the mask to subset prices
+# outliers = prices[is_over_3]
 
-print(len(outliers))s
+# print(len(outliers))s
